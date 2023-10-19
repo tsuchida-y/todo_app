1c5317b8413ea59fbea86b48dc275244d62ed237
tsuchida <yu41926t.tutida@icloud.com>
2023-10-19 18:35:12 +0900

    警告の解消

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   // 最初に表示するWidget
-  runApp(MyTodoApp());
+  runApp(const MyTodoApp());
 }
 
 class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,18 +21,52 @@ class MyTodoApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // リスト一覧画面を表示
-      home: TodoListPage(),
+      home: const TodoListPage(),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      autofocus: true, //TextFieldが表示されるときにフォーカスする（キーボードを表示する）
+      cursorColor: Colors.white, //カーソルの色
+      style: TextStyle( //テキストのスタイル
+        color: Colors.white,
+        fontSize: 20,
+      ),
+      textInputAction: TextInputAction.search, //キーボードのアクションボタンを指定
+      decoration: InputDecoration( //TextFiledのスタイル
+        enabledBorder: UnderlineInputBorder( //デフォルトのTextFieldの枠線
+          borderSide: BorderSide(color: Colors.white)
+        ),
+        focusedBorder: UnderlineInputBorder( //TextFieldにフォーカス時の枠線
+          borderSide: BorderSide(color: Colors.white)
+        ),
+        hintText: 'Search', //何も入力してないときに表示されるテキスト
+        hintStyle: TextStyle( //hintTextのスタイル
+          color: Colors.white60,
+          fontSize: 20,
+        ),
+      ),
     );
   }
 }
 
 // リスト一覧画面用Widget
 class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
   @override
-  _TodoListPageState createState() => _TodoListPageState();
+  TodoListPageState createState() => TodoListPageState();
 }
 
-class _TodoListPageState extends State<TodoListPage> {
+class TodoListPageState extends State<TodoListPage> {
   // Todoリストのデータ
   List<String> todoList = [];
 
@@ -39,18 +75,30 @@ class _TodoListPageState extends State<TodoListPage> {
     return Scaffold(
       // AppBarを表示し、タイトルも設定
       appBar: AppBar(
-        title: Text('リスト一覧'),
+        title: const Text('リスト'),
       ),
       // データを元にListViewを作成
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(todoList[index]),
-            ),
-          );
-        },
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){},
+          style: ElevatedButton.styleFrom(
+          fixedSize: const Size(20, 20),
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.all(2),
+          ),
+          child: const Text("ボタン"),
+          ),
+          ListView.builder(
+            itemCount: todoList.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(todoList[index]),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -59,7 +107,7 @@ class _TodoListPageState extends State<TodoListPage> {
           final newListText = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               // 遷移先の画面としてリスト追加画面を指定
-              return TodoAddPage();
+              return const TodoAddPage();
             }),
           );
           if (newListText != null) {
@@ -70,18 +118,21 @@ class _TodoListPageState extends State<TodoListPage> {
             });
           }
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
+        
       ),
     );
   }
 }
 
 class TodoAddPage extends StatefulWidget {
+  const TodoAddPage({super.key});
+
   @override
-  _TodoAddPageState createState() => _TodoAddPageState();
+  TodoAddPageState createState() => TodoAddPageState();
 }
 
-class _TodoAddPageState extends State<TodoAddPage> {
+class TodoAddPageState extends State<TodoAddPage> {
   // 入力されたテキストをデータとして持つ
   String _text = '';
 
@@ -90,16 +141,16 @@ class _TodoAddPageState extends State<TodoAddPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('リスト追加'),
+        title: const Text('リスト追加'),
       ),
       body: Container(
         // 余白を付ける
-        padding: EdgeInsets.all(64),
+        padding: const EdgeInsets.all(64),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // 入力されたテキストを表示
-            Text(_text, style: TextStyle(color: Colors.blue)),
+            Text(_text, style: const TextStyle(color: Colors.blue)),
             const SizedBox(height: 8),
             // テキスト入力
             TextField(
@@ -113,7 +164,7 @@ class _TodoAddPageState extends State<TodoAddPage> {
               },
             ),
             const SizedBox(height: 8),
-            Container(
+            SizedBox(
               // 横幅いっぱいに広げる
               width: double.infinity,
               // リスト追加ボタン
@@ -123,11 +174,11 @@ class _TodoAddPageState extends State<TodoAddPage> {
                   // "pop"の引数から前の画面にデータを渡す
                   Navigator.of(context).pop(_text);
                 },
-                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+                child: const Text('リスト追加', style: TextStyle(color: Colors.white)),
               ),
             ),
             const SizedBox(height: 8),
-            Container(
+            SizedBox(
               // 横幅いっぱいに広げる
               width: double.infinity,
               // キャンセルボタン
@@ -137,7 +188,7 @@ class _TodoAddPageState extends State<TodoAddPage> {
                   // "pop"で前の画面に戻る
                   Navigator.of(context).pop();
                 },
-                child: Text('キャンセル'),
+                child: const Text('キャンセル'),
               ),
             ),
           ],
