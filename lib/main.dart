30a326ba8e7bdc9572ba73040d53fdcd237cb3e5
tsuchida <yu41926t.tutida@icloud.com>
2023-10-25 19:52:34 +0900

    new widgetを削除

--- lib/main.dart
+++ lib/main.dart
@@ -26,44 +26,6 @@ class MyTodoApp extends StatelessWidget {
   }
 }
 
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    super.key,
-  });
-
-  @override //tabバー
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0, // 最初に表示するタブ
-      length: 3, // タブの数
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(text: '野球'),
-              Tab(text: 'サッカー'),
-              Tab(text: 'テニス'),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(
-              child: Text('野球', style: TextStyle(fontSize: 32.0)),
-            ),
-            Center(
-              child: Text('サッカー', style: TextStyle(fontSize: 32.0)),
-            ),
-            Center(
-              child: Text('テニス', style: TextStyle(fontSize: 32.0)),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 // リスト一覧画面用Widget
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
